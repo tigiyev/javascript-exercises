443c21e877451e76e2d3b51450fd0b15913c9977
Timur <tigiyev@gmail.com>
2026-04-07 20:12:41 +0400

    progress 10,11

--- foundations/10_leapYears/leapYears.js
+++ foundations/10_leapYears/leapYears.js
@@ -1,6 +1,21 @@
-const leapYears = function() {
+const leapYears = function (year) {
+
+	// devided by 4 +
+	// devided by 100 -
+	// devided by 400 +
+
+	if (year % 4 === 0 && (year % 100 !== 0 || year % 400 === 0)) {
+		console.log(year + " is leap year!");
+		return true
+	}
+	else {
+		console.log(year + " is NOT leap year!");
+		return false
+	}
 
 };
+
+// leapYears(1996)
 
 // Do not edit below this line
 module.exports = leapYears;
--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,8 +1,18 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (F) {
+
+  let C = +((F - 32) * 5 / 9).toFixed(1);
+  return C
 };
 
-const convertToFahrenheit = function() {
+// convertToCelsius(-73.3)
+
+const convertToFahrenheit = function (C) {
+
+  let F = +(C * 9 / 5 + 32).toFixed(1);
+  return F
 };
+
+// convertToFahrenheit(0)
 
 // Do not edit below this line
 module.exports = {
