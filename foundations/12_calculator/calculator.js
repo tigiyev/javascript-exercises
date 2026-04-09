022f6b2df7fcc8b2193143be0ed20967ee359039
Timur Tigiyev <tigiyev@gmail.com>
2026-04-09 18:45:42 +0400

    progress 12, 13

--- foundations/12_calculator/calculator.js
+++ foundations/12_calculator/calculator.js
@@ -1,26 +1,52 @@
-const add = function() {
-	
+const add = function (a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function (a, b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function (array) {
+  if (array.length === 0) return 0;
+
+  function reducer(accumulator, currentValue) {
+    return accumulator + currentValue;
+  }
+  return array.reduce(reducer)
 };
 
-const multiply = function() {
+const multiply = function (array) {
+  if (array.length === 0) return 0;
 
+  function reducer(accumulator, currentValue) {
+    return accumulator * currentValue;
+  }
+  return array.reduce(reducer)
 };
 
-const power = function() {
-	
+const power = function (a, b) {
+  return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function (a) {
+
+  if (a === 0) return 1;
+
+
+  const array = [];
+  for (let i = a; i > 0; i--) {
+    array.push(i)
+  }
+
+
+  function reducer(accumulator, currentValue) {
+    return accumulator * currentValue;
+  }
+  return array.reduce(reducer)
+
+
 };
+factorial(3);
 
 // Do not edit below this line
 module.exports = {
--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,6 +1,21 @@
-const palindromes = function () {
+const palindromes = function (string) {
 
+	// remove punctuation (" ", ",", ".", "!")
+
+	// v1
+	// let clean = string.split(' ').join('').split(',').join('').split('.').join('').split('!').join('').toLowerCase();
+	// console.log(clean);
+
+	// v2 
+	let clean = string.split(/[,. !]/).join('').toLowerCase();
+
+	let reversed = clean.split('').reverse().join('')
+	console.log(reversed);
+
+	return clean === reversed;
 };
+
+palindromes('A car, a man, a maraca.')
 
 // Do not edit below this line
 module.exports = palindromes;
